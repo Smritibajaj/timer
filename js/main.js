12c36a1665c6d03a80cc6547d59b23d9d464502e
mohit <mohit.batra@spicedigital.com>
2018-07-30 03:27:30 +0530

    improvements

--- js/main.js
+++ js/main.js
@@ -28,6 +28,23 @@ var x = setInterval(function() {
         document.getElementById("demo1").innerHTML = "EXPIRED";
     }
 }, 1000);
+var incrimentTime ="";
+var addon = countDownDate;
+var incrimentday = countDownDate;
+setInterval(function()
+    {
+        var now = new Date().getTime();       
+        var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var incrimentTime = seconds + 5;
+        document.getElementById("demo2").innerHTML = days + "d " + hours + "h "
+     + minutes + "m " + incrimentTime + "s ";
+
+
+}, 5000);
 
 function myFunction() {
     document.getElementById('wrapper').style.cssText = 'background-color: red;'
