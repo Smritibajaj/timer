adc9cce139026ea3bce36afe6df869744e6c09c5
mohit <mohit.batra@spicedigital.com>
2018-07-30 02:08:50 +0530

    initial commit for demo

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var d = new Date();
+
+var countDownDate = new Date("Jan 30, 2019 12:00:00").getTime();
+
+
+var x = setInterval(function() {
+
+    
+    var now = new Date().getTime();
+    
+    
+    var distance = countDownDate - now;
+    
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+   
+    document.getElementById("demo").innerHTML = d;
+    document.getElementById("demo1").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo1").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+function myFunction() {
+    document.getElementById('wrapper').style.cssText = 'background-color: red;'
+}
